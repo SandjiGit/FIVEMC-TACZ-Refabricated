5c8604f6b3d577bbfab905557c4f7e0f358e8e04
mikasa <1244456115@qq.com>
2025-09-06 23:19:50 +0800

    Fix CustomRenderer.

--- src/main/java/cn/sh1rocu/tacz/client/TaCZFabricClient.java
+++ src/main/java/cn/sh1rocu/tacz/client/TaCZFabricClient.java
@@ -1,6 +1,7 @@
 package cn.sh1rocu.tacz.client;
 
 import cn.sh1rocu.tacz.api.event.*;
+import cn.sh1rocu.tacz.api.extension.IItem;
 import com.tacz.guns.api.client.event.BeforeRenderHandEvent;
 import com.tacz.guns.api.client.event.RenderItemInHandBobEvent;
 import com.tacz.guns.api.client.event.SwapItemWithOffHand;
@@ -22,6 +23,8 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientConfigurationNetworking;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
+import net.minecraft.core.registries.BuiltInRegistries;
 
 public class TaCZFabricClient implements ClientModInitializer {
     @Override
@@ -33,6 +36,10 @@ public class TaCZFabricClient implements ClientModInitializer {
         ModContainerScreen.registerScreens();
         ModEntitiesRender.registerEntityRenderers();
         ParticleFactories.registerParticles();
+        BuiltInRegistries.ITEM.stream().filter(item -> item instanceof IItem).forEach(clientEx ->
+                BuiltinItemRendererRegistry.INSTANCE.register(clientEx,
+                        (stack, mode, matrices, vertexConsumers, light, overlay) ->
+                                ((IItem) clientEx).getCustomRenderer().renderByItem(stack, mode, matrices, vertexConsumers, light, overlay)));
         subscribeEvents();
     }
 
