7e6049af0dbbaf0163da394ccd6a14bc6d8fd9ab
mikasa <1244456115@qq.com>
2025-12-31 04:33:37 +0800

    WTF, why did i comment this???

--- src/main/java/com/tacz/guns/compat/controllable/ControllableCompat.java
+++ src/main/java/com/tacz/guns/compat/controllable/ControllableCompat.java
@@ -1,11 +1,13 @@
 package com.tacz.guns.compat.controllable;
 
+import net.fabricmc.loader.api.FabricLoader;
+
 public class ControllableCompat {
     private static final String MOD_ID = "controllable";
 
     public static void init() {
-//        if (FabricLoader.getInstance().isModLoaded(MOD_ID)) {
-//            ControllableInner.init();
-//        }
+        if (FabricLoader.getInstance().isModLoaded(MOD_ID)) {
+            ControllableInner.init();
+        }
     }
 }
