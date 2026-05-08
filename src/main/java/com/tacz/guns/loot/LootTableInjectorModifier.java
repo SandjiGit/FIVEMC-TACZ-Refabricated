b3bf589378c88cbea9a07f2fa55850c2b54950a6
mikasa <1244456115@qq.com>
2026-05-08 14:08:32 +0800

    fix: global modifier

--- src/main/java/com/tacz/guns/loot/LootTableInjectorModifier.java
+++ src/main/java/com/tacz/guns/loot/LootTableInjectorModifier.java
@@ -2,26 +2,36 @@ package com.tacz.guns.loot;
 
 import com.tacz.guns.resource.CommonAssetsManager;
 import com.tacz.guns.resource.pojo.data.loot.LootTableInjection;
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootTable;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class LootTableInjectorModifier {
-    public static void init() {
-        // Global Modifier
-        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            CommonAssetsManager manager = CommonAssetsManager.getInstance();
-            if (manager == null) {
-                return;
-            }
-            List<LootTableInjection> injections = manager.getLootTableInjections(id);
-            if (injections.isEmpty()) {
-                return;
-            }
-            for (LootTableInjection injection : injections) {
-                tableBuilder.pools(Arrays.asList(injection.lootTable().pools));
-            }
-        });
+    public static @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context, LootTable table) {
+        CommonAssetsManager manager = CommonAssetsManager.getInstance();
+        if (manager == null) {
+            return generatedLoot;
+        }
+
+        ResourceLocation lootTableId = context.getLevel().getServer().reloadableRegistries().get().registryOrThrow(Registries.LOOT_TABLE).getKey(table);
+        if (lootTableId == null) {
+            return generatedLoot;
+        }
+
+        List<LootTableInjection> injections = manager.getLootTableInjections(lootTableId);
+        if (injections.isEmpty()) {
+            return generatedLoot;
+        }
+
+        for (LootTableInjection injection : injections) {
+            generatedLoot.addAll(injection.createStacks(context));
+        }
+        return generatedLoot;
     }
 }
